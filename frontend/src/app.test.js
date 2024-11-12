13fba2b4897a77cdf9d0fdd9c919838712c33b48
EduardoTocci <eduardo.lago.tocci@gmail.com>
2024-11-12 09:45:18 -0300

    alteração no package e criação de arquivos de teste

---
+++ frontend/src/app.test.js
@@ -0,0 +1,7 @@
+// src/app.test.js
+describe('Teste básico do Jest', () => {
+    it('deve passar este teste', () => {
+      expect(true).toBe(true);  // Um simples teste que sempre passa
+    });
+  });
+  
